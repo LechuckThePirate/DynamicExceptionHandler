b5521deecdeaf0a527ace847f1c38e9ee6643dca
joanvilarino <joan.vilarino@gmail.com>
2015-10-14 09:21:22 +0200

    Initial commit

---
+++ DynamicExceptionHandler/DynExceptionHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DynamicExceptionHandler
+{
+    public class DynExceptionHandler
+    {
+
+        private static void DynHandle(NullReferenceException ex)
+        {
+            Console.WriteLine("Null reference exception thrown!");
+            Trace.TraceError("{0}",ex);
+        }
+
+        private static void DynHandle(DivideByZeroException ex)
+        {
+            Console.WriteLine("Divide by zero exception thrown!");
+            Trace.TraceError("{0}", ex);
+        }
+
+        private static void DynHandle(NotImplementedException ex)
+        {
+            Console.WriteLine("Not implemented exception thrown!");
+            Trace.TraceError("{0}", ex);
+        }
+
+        private static void DynHandle(Exception ex)
+        {
+            Console.WriteLine("Unknown exception type! {0}", ex.GetType());
+            Trace.TraceError("{0}",ex);
+        }
+
+        private static void DynHandle(object ex)
+        {
+            Console.WriteLine("Not an exception!!! {0}", ex.GetType());
+            Trace.TraceError("{0}", ex);
+        }
+
+        public static void HandleException(dynamic ex)
+        {
+            if (ex == null) 
+                DynHandle(new Exception("Null Exception thrown?",ex));
+            DynHandle(ex);
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+
+    }
+}
---
+++ DynamicExceptionHandler/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DynamicExceptionHandler
+{
+    class Program
+    {
+
+        private static void GenerateNullReference()
+        {
+            Console.WriteLine(">>> Generating NullReferenceException");
+            string a = null;
+            var b = a.ToString();
+        }
+
+        private static void GenerateDivideByZero()
+        {
+            Console.WriteLine(">>> Generating DivideByZeroException");
+            var i = 0;
+            var j = 100 / i;
+        }
+
+        private static void GenerateNotImplemented()
+        {
+            Console.WriteLine(">>> Generating NotImplementedException");
+            throw new NotImplementedException();
+        }
+
+        private static ConsoleKeyInfo ReadOption()
+        {
+            Console.WriteLine("Select 1, 2 or 3 to generate a exception. 4 to exit");
+            var result = Console.ReadKey();
+            Console.WriteLine();
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+
+            ConsoleKeyInfo k = new ConsoleKeyInfo();
+            do
+            {
+                try
+                {
+                    k = ReadOption();
+                    switch (k.KeyChar)
+                    {
+                        case '1':
+                            GenerateNullReference();
+                            break;
+                        case '2':
+                            GenerateDivideByZero();
+                            break;
+                        case '3':
+                            GenerateNotImplemented();
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    DynExceptionHandler.HandleException(ex);
+                }
+            } while (k.KeyChar != '4');
+
+
+        }
+    }
+}
